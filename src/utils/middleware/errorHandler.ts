09add813f377ba7d53388789babd7bb31711d5bb
SaatwikRishi <saatwiksanjay@gmail.com>
2023-07-24 01:01:46 +0530

    fix: error handler with prisma errors

--- src/utils/middleware/errorHandler.ts
+++ src/utils/middleware/errorHandler.ts
@@ -1,5 +1,19 @@
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import { Request, Response } from "express";
-const errorHandler = (error: any, req: Request, res: Response) => {
-    res.send(error);
+const errorHandler = (error: any, req: Request, res: Response, next: any) => {
+  if (error instanceof PrismaClientKnownRequestError) {
+    if (["P1001", "P1017", "P1002"].includes(error.code)) {
+      return res.status(404).json({
+        message: "Something went wrong",
+      });
+    }
+    return res.status(400).json({
+      message: error.meta?.cause ?? error.meta ?? "Bad request",
+    });
+  } else {
+    return res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
 };
 export default errorHandler;
